41762e8b5e24da6119530806f0e026433f66e5f6
kangsuroro <kangsuroro2907@gmail.com>
2020-04-18 19:14:13 +0900

    add getUserInfo service method

--- src/app/services/user-info.service.ts
+++ src/app/services/user-info.service.ts
@@ -1,10 +1,46 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpParams, HttpResponse, HttpErrorResponse } from '@angular/common/http';
+import { UserList } from '../models';
+import { tap, retry, finalize, switchAll, catchError} from 'rxjs/operators';
+import { Observable, throwError } from 'rxjs';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class UserInfoService {
+  apiUrl:string = 'https://api.github.com/search/users';
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient) {}
+
+  getUserInfo(word: string, showloadFunc, hideLoadFunc): Observable<UserList> {
+    const params = new HttpParams().set('q', word);
+
+    return this.http.get<UserList>(this.apiUrl, { params })
+      .pipe(
+        tap(showloadFunc),
+        catchError(this.handleError),
+        switchAll<UserList>(),
+        tap(hideLoadFunc),
+        retry(2),
+        finalize(hideLoadFunc),
+      )
+  }
+
+  private handleError(error: HttpErrorResponse){
+    let message: string = '';
+    // client 에러
+    if(error.error instanceof ErrorEvent){
+      console.error(`Client-side error: ${error.error.message}`);
+      message = error.error.message;
+    }else{
+      // server 에러
+      console.error(`Server-side error: ${error.status}`);
+      message = error.message;
+    }
+
+    return throwError({
+      title: 'sorry, something wrong',
+      message
+    })
+  }
 }
